18f56905025ccc587c6b0563edd1dfa1e0fab7b5
Fernando <josefernandobarros@gmail.com>
2023-09-01 18:45:17 -0300

    Combios en login

--- Sistema Sociedad Italiana/My Project/Application.Designer.vb
+++ Sistema Sociedad Italiana/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Sistema_Sociedad_Italiana.Login
+            Me.MainForm = Global.Sistema_Sociedad_Italiana.Nuevo_Usuario
         End Sub
     End Class
 End Namespace
